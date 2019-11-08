42c65f4454fd98edb5b35d79516c2b1ceffe8628
Blake Hartley <30282035+blakehartley@users.noreply.github.com>
2019-11-08 08:07:26 -0500

    Compression completed

--- gym_map_bro/envs/map_bro_env.py
+++ gym_map_bro/envs/map_bro_env.py
@@ -13,6 +13,7 @@ class broEnv(gym.Env):
 	# __init__ is essentially pointless, the program is designed to be run from __myinit__
 	def __init__(self):
 		self.action_space = spaces.Discrete(3)
+		self.compress_frac = 0.5
 		self.observation_space = spaces.Discrete(3)
 		N_database = 1
 		N_batch = 1
@@ -37,6 +38,9 @@ class broEnv(gym.Env):
 		# 1 = Compress
 		# 2 = Delete
 		self.action_space = spaces.Discrete(3)
+
+		# Compression value fraction
+		self.compress_frac = 0.5
 		
 		# Define size variables
 		self.N_database = N_database
@@ -49,7 +53,8 @@ class broEnv(gym.Env):
 		# Read the column names from dns.col
 		col = pandas.read_csv("dns.col")
 		self.col = col
-		# Define the blank DataFrame
+		# Define the blank DataFrames
+		# df0 is for "saving" while df1 is for "compressing"
 		self.df0 = pandas.DataFrame(index=index, columns=col.columns)
 		self.df1 = pandas.DataFrame(index=index, columns=col.columns)
 		
@@ -58,12 +63,17 @@ class broEnv(gym.Env):
 		# 1: Positive reward for adding the row to the table
 		if len(init_s) == 0:
 			self.values0 = np.zeros((N_database,2))
+			self.values1 = np.zeros((N_database,2))
+
 			# Make copy of initial value table before playing the game
 			self.values0_init = np.zeros((N_database,2))
+			self.values1_init = np.zeros((N_database,2))
 		else:
-			self.values0 = init_s
-			# Make copy of initial value table before playing the game
-			self.values0_init = init_s
+			self.values0 = init_s[0]
+			self.values1 = init_s[1]
+			
+			self.values0_init = init_s[0]
+			self.values1_init = init_s[1]
 
 		# Steps/Observations #
 		# A single step is trying to save/delete/etc. a single line from the batch
@@ -74,14 +84,23 @@ class broEnv(gym.Env):
 	# Full reset. Reset steps, database, and values
 	def reset(self):
 		self.step_num = 0
+
 		self.df0 = pandas.DataFrame(index=self.index, columns=self.col.columns)
+		self.df1 = pandas.DataFrame(index=self.index, columns=self.col.columns)
+
 		self.values0 = np.zeros((self.N_database,2))
+		self.values1 = np.zeros((self.N_database,2))
+
+		self.values0_init = np.zeros((self.N_database,2))
+		self.values1_init = np.zeros((self.N_database,2))
+
 		return self.step_num
 	
 	# Batch reset. Used to start trying to save a new batch of lines
 	def batch_reset(self):
 		self.step_num = 0
 		self.values0 = np.copy(self.values0_init)
+		self.values1 = np.copy(self.values1_init)
 		return self.step_num
 	
 	# An action is defined by:
@@ -89,19 +108,29 @@ class broEnv(gym.Env):
 	# Delete: do nothing to the value table, lose value of deleted line as negative reward
 	def _take_action(self, action, value):
 		reward = 0
-		#print(f'start of action {values0}')
-		if(action == 0):
+		if(action == 0):	# Save to df0 with full reward
 			# Find the lowest value of the value table
 			# axis=0 minimizes over columns, [1] is the column of values
 			val_arg = np.argmin(self.values0, axis=0)[1]
 			old_val = self.values0[val_arg][1]
+
 			# Reward is the new value minus the old value
 			# New value replaces old value
 			reward = value - old_val
 			self.values0[val_arg][1] = value
-		elif(action == 1):
+		if(action == 1):	# Save to df1 with discounted reward
+			# Find the lowest value of the value table
+			# axis=0 minimizes over columns, [1] is the column of values
+			val_arg = np.argmin(self.values1, axis=0)[1]
+			old_val = self.values1[val_arg][1]
+
+			# Reward is the new value minus the old value
+			# New value replaces old value
+			discount_value = self.compress_frac*value
+			reward = discount_value - old_val
+			self.values1[val_arg][1] = discount_value
+		elif(action == 2):	# Delete with negative value reward
 			reward = -value
-		#print(f'end of action {values0}')
 		
 		return reward
 	
@@ -116,10 +145,10 @@ class broEnv(gym.Env):
 	
 	# Decay function for valeus
 	# The value table will contain the current
-	def decay_step(self):
-		self.values0_init[:,1] = self.values0_init[:,1]*0.9
-	def inv_decay(self, val, n):
-		return val*0.9**(-n)
+	def decay_step(self, val, rate):
+		val = val*rate
+	def inv_decay(self, val, n, rate):
+		return val*rate**(-n)
 	
 	# The time step is where we complete the actions recommended by the agent.
 	# 1. Loop through recommendations and apply to database
@@ -141,25 +170,53 @@ class broEnv(gym.Env):
 				dns_batch = pandas.read_csv("dns.log")
 				dns_line = dns_batch.values[i]
 				self.df0.loc[rep_row] = dns_line
+			if(actions[i] == 1):
+				# Find the row we want to replace
+				rep_row = np.argmin(self.values1_init, axis=0)[1]
+				
+				# Replace the value row
+				self.values1_init[rep_row, 0] = 0
+				self.values1_init[rep_row, 1] = self.compress_frac*values[i]
+
+				# Replace the database row
+				dns_batch = pandas.read_csv("dns.log")
+				dns_line = dns_batch.values[i]
+				self.df1.loc[rep_row] = dns_line
 		
-		self.decay_step()
+		self.decay_step(self.values0_init[:,1], 0.9)
+		self.decay_step(self.values1_init[:,1], 0.9)
+
 		self.values0_init[:,0] += 1
+		self.values1_init[:,0] += 1
 		
 		#new_values = self.values0
 		#self.values0_init = new_values
 	
 	def render(self, mode='human', close=True):
-		time = self.values0_init[:,0]
-		value = self.inv_decay(self.values0_init[:,1],self.values0_init[:,0])
-		plt.scatter(time, value, alpha=1.0)
+		time0 = self.values0_init[:,0]
+		value0 = self.inv_decay(self.values0_init[:,1],self.values0_init[:,0], 0.9)
+		plt.scatter(time0, value0, color='b', alpha=1.0, label="Uncompressed")
+
+		time1 = self.values1_init[:,0]
+		value1 = self.inv_decay(self.values1_init[:,1],self.values1_init[:,0], 0.9)
+		plt.scatter(time1, value1, color='r', alpha=1.0, label="Compressed")
+
 		plt.title('Age vs Value')
 		plt.xlabel('Age')
 		plt.ylabel('Value')
+		plt.legend(loc=2)
 		plt.show()
 		plt.close()
 
 		print_df = self.df0.copy()
-		print_df['age'] = time
-		print_df['value'] = value
+		print_df['age'] = time0
+		print_df['value'] = value0
+		print("Uncompressed Database:")
+		print(print_df[['uid', 'src', 'sport', 'dst', 'age', 'value']])
+
+		print_df = self.df1.copy()
+		print_df['age'] = time1
+		print_df['value'] = value1
+		print("Compressed Database:")
 		print(print_df[['uid', 'src', 'sport', 'dst', 'age', 'value']])
 		return 0
